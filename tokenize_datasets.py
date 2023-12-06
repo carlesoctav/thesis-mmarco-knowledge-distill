ea9da46740db4f55bb2c9a9f085f486ab332757b
carlesoctav <carlesoctavianus@tuta.io>
2023-12-06 07:38:47 +0000

    new commit aja gatau apa :D

--- tokenize_datasets.py
+++ tokenize_datasets.py
@@ -6,25 +6,26 @@ from sentence_transformers import SentenceTransformer
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-student_tokenizer = AutoTokenizer.from_pretrained("distilbert-base-multilingual-cased")
-parent_tokenizer = AutoTokenizer.from_pretrained("sentence-transformers/msmarco-distilbert-base-tas-b")
-parent_model = AutoModel.from_pretrained("sentence-transformers/msmarco-distilbert-base-tas-b")
+student_tokenizer = AutoTokenizer.from_pretrained("bert-base-multilingual-uncased")
+parent_tokenizer = AutoTokenizer.from_pretrained("sentence-transformers/msmarco-bert-base-dot-v5")
+parent_model = AutoModel.from_pretrained("sentence-transformers/msmarco-bert-base-dot-v5")
 
 def embedding(datasets, parent_model, parent_tokenizer):
 
-    def cls_pooling(model_output):
-        return model_output.last_hidden_state[:,0]
+    def mean_pooling(model_output, attention_mask):
+        token_embeddings = model_output.last_hidden_state
+        input_mask_expanded = attention_mask.unsqueeze(-1).expand(token_embeddings.size()).float()
+        return torch.sum(token_embeddings * input_mask_expanded, 1) / torch.clamp(input_mask_expanded.sum(1), min=1e-9)
 
     parent_model.to(device)
     
-
     def embedding_batch(examples):
         encoded_input = parent_tokenizer(examples["text_en"], padding="max_length", truncation=True, max_length=256, return_tensors="pt")
         encoded_input = encoded_input.to(device)
         with torch.no_grad():
             model_output = parent_model(**encoded_input)
 
-        target_embedding = cls_pooling(model_output).detach().cpu().numpy()
+        target_embedding = mean_pooling(model_output, encoded_input["attention_mask"]).detach().cpu().numpy()
 
         return {
             "target_embedding": target_embedding
@@ -34,10 +35,6 @@ def embedding(datasets, parent_model, parent_tokenizer):
     return embedding_datasets
 
         
-
-
-
-
 def tokenize(datasets, student_tokenizer):
     """
     datasets: huggingface datasets
@@ -53,11 +50,13 @@ def tokenize(datasets, student_tokenizer):
         return {
             "input_ids_en": output_en.input_ids,
             "attention_mask_en": output_en.attention_mask,
+            "token_type_ids_en": output_en.token_type_ids, 
             "input_ids_id": output_id.input_ids,
             "attention_mask_id": output_id.attention_mask,
+            "token_type_ids_id": output_id.token_type_ids,
         }
 
-    tokenized_datasets = datasets.map(tokenize_batch, batched=True, num_proc=8)
+    tokenized_datasets = datasets.map(tokenize_batch, batched=True, num_proc=16)
     return tokenized_datasets
 
 
@@ -67,4 +66,4 @@ dataset = load_dataset("carles-undergrad-thesis/en-id-parallel-sentences")
 
 embedding_dataset = embedding(dataset, parent_model, parent_tokenizer)
 embedding_tokenized_dataset = tokenize(embedding_dataset, student_tokenizer)
-embedding_tokenized_dataset.push_to_hub("carles-undergrad-thesis/en-id-parallel-sentences-embedding")
+embedding_tokenized_dataset.push_to_hub("carles-undergrad-thesis/en-id-parallel-sentences-embedding-vbert")
